c0d82887b0f7638a637be5eeea7e8e82d7d87ff7
azazaikens <rubanjuri@gmail.com>
2025-08-11 22:11:32 +0300

    start project

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <img src="../../assets/login-money-desktop.png" alt="maney" />
+        <form action="">
+          
+        </form>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+    return (
+        <header className="bg-[#1F1F27] w-full h-14 flex items-center">
+            <img src="./logo.svg" alt="boss logo" className="w-[170px] h-[40]" />
+        </header>
+    )
+}
